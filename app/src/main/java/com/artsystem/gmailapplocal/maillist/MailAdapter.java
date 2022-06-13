9170f4c75464f28e574415e35abb6ccbdfc0d088
NguyenHieu201 <hieu.nguyenminh2012001@gmail.com>
2022-06-14 00:19:37 +0700

    Create Divider

--- app/src/main/java/com/artsystem/gmailapplocal/maillist/MailAdapter.java
+++ app/src/main/java/com/artsystem/gmailapplocal/maillist/MailAdapter.java
@@ -30,9 +30,6 @@ public class MailAdapter extends ListAdapter<MailItem, MailAdapter.MailBaseViewH
     public MailBaseViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
 
         switch (viewType) {
-            case Constant.MAIL_CATEGORY_TYPE :
-                return new CatViewHolder (LayoutInflater.from(parent.getContext())
-                        .inflate(R.layout.item_category,parent,false));
             case Constant.MAIL_ITEM_TYPE:
                 return new MailViewHolder (LayoutInflater.from(parent.getContext())
                         .inflate(R.layout.item_mail,parent,false));
@@ -65,64 +62,6 @@ public class MailAdapter extends ListAdapter<MailItem, MailAdapter.MailBaseViewH
             super(itemView);
         }
     }
-
-
-
-    // categorie item ViewHolder
-    public class CatViewHolder extends MailBaseViewHolder{
-
-        TextView tvTitle,tvDesc,tvContent,tvNum;
-        ImageView imgCat;
-
-
-        public CatViewHolder(@NonNull View itemView) {
-            super(itemView);
-
-            tvTitle = itemView.findViewById(R.id.item_cat_title);
-            //tvContent = itemView.findViewById(R.id.item_mail_content);
-            tvDesc = itemView.findViewById(R.id.item_cat_description);
-            tvNum = itemView.findViewById(R.id.item_cat_counter);
-
-            imgCat = itemView.findViewById(R.id.item_cat_img);
-        }
-
-        @Override
-        void bindData(MailItem item) {
-
-            tvTitle.setText(item.getCategoryItem().getTitle());
-            tvDesc.setText(item.getCategoryItem().getDesc());
-            tvNum.setText(item.getCategoryItem().getNumNotif()+" new");
-            Glide.with(imgCat.getContext()).load(item.getCategoryItem().getIcon()).into(imgCat);
-            switch (item.getCategoryItem().getColor()) {
-
-                case "GREEN" :
-                    imgCat.setColorFilter(imgCat.getContext().getResources().getColor(R.color.green));
-                    DrawableCompat.setTint(tvNum.getBackground(),imgCat.getContext().getResources().getColor(R.color.green));
-                    break;
-                case "BLACK" :
-                    imgCat.setColorFilter(imgCat.getContext().getResources().getColor(R.color.black));
-                    DrawableCompat.setTint(tvNum.getBackground(),imgCat.getContext().getResources().getColor(R.color.black));
-                    break;
-                case "YELLOW" :
-                    imgCat.setColorFilter(imgCat.getContext().getResources().getColor(R.color.yellow));
-                    DrawableCompat.setTint(tvNum.getBackground(),imgCat.getContext().getResources().getColor(R.color.yellow));
-                    break;
-                case "BLUE" :
-                    imgCat.setColorFilter(imgCat.getContext().getResources().getColor(R.color.blue));
-                    DrawableCompat.setTint(tvNum.getBackground(),imgCat.getContext().getResources().getColor(R.color.blue));
-                    break;
-                case "PURPLE" :
-                    imgCat.setColorFilter(imgCat.getContext().getResources().getColor(R.color.purple));
-                    DrawableCompat.setTint(tvNum.getBackground(),imgCat.getContext().getResources().getColor(R.color.purple));
-                    break;
-                default: tvNum.setBackgroundColor(imgCat.getContext().getResources().getColor(R.color.red));
-
-            }
-
-        }
-    }
-
-
 
     // mail item viewholder
     public class MailViewHolder extends MailBaseViewHolder{
